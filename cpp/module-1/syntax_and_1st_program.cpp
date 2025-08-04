db12395eae865563f6c4ae7a5699476d99e25a77
HUZAIFA-BUKHARI-DEV <huzaifabukhari.dev.designer@gmail.com>
2025-08-04 13:06:03 +0500

    syntax

--- cpp/module-1/syntax_and_1st_program.cpp
+++
@@ -1,5 +0,0 @@
-// <- what are these two lines? structure or main syntax ? No Nope Not-at-all, not a structure but a syntax of something :) ok.K then explain it . oH yeah Sure :::::::: done
-
-
-
-
---
+++ cpp_practice/syntax.cpp
@@ -0,0 +1,16 @@
+// This is a simple C++ program to demonstrate basic syntax.
+
+
+// #include <bits/stdc++.h> // Not recommended for C++17, use specific headers instead like <iostream>, <vector>, etc.
+#include <iostream> // Include the iostream header for input/output operations
+using namespace std; // Use the standard namespace to avoid prefixing std:: before standard functions and objects
+int main() { // Main function where the program execution starts
+
+    cout << "Hello, World!" << endl; // this prints "Hello, World!" to the console
+
+    return 0; // Return 0 indicates that the program ended successfully
+}// End of the main function
+
+
+
+// wait ! what the heck are these // two lines everywhere?  Aah ! got it  ..... these // two lines are called Comments. Are used just to make the code understandable, like normal notes made to understand complex physics , maths , english etc topics.
